c48544f9c66945db295a4c845aee1accdd9ec62a
machapraveen <praveenmacha777@gmail.com>
2024-08-09 06:46:32 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -26,4 +26,5 @@ app.use('/api/invoices', invoiceRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
